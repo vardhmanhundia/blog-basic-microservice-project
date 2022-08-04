5fac8d3e7d8a853d80e8e713674138ff5ec2b4c2
Vardhman Hundia <vardhmanhundia@gmail.com>
2022-08-05 04:50:36 +0530

    chore(posts.com): chore add posts.com as default url

--- event-bus/index.js
+++ event-bus/index.js
@@ -12,16 +12,16 @@ app.post("/events", (req, res) => {
 
   events.push(event);
 
-  axios.post("http://posts-clusterip-srv:4000/events", event).catch((err) => {
+  axios.post("http://posts.com/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://comments-srv:4001/events", event).catch((err) => {
+  axios.post("http://posts.com", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://query-srv:4002/events", event).catch((err) => {
+  axios.post("http://posts.com/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://moderation-srv:4003/events", event).catch((err) => {
+  axios.post("http://posts.com/events", event).catch((err) => {
     console.log(err.message);
   });
   res.send({ status: "OK" });
